29f535f54a70193844a1a89df3e9fafa7820e296
kandl25 <war21039@byui.edu>
2024-06-24 15:53:22 -0600

    Develope 3 base

---
+++ prove/Develop03/Scripture.cs
@@ -0,0 +1,24 @@
+using System;
+class Scripture
+{
+    private Reference _reference;
+    private List<Word> _words = new List<Word>();
+
+    public Scripture(Reference Reference, string text)
+    {
+
+    }
+
+    public void HideRandomWords(int numberToHide)
+    {
+
+    }
+    public string GetDisplayText()
+    {
+        return;
+    }
+    public bool IsCompletelyHidden()
+    {
+
+    }
+}
---
+++ prove/Develop03/Word.cs
@@ -0,0 +1,27 @@
+using System;
+class Word
+{
+    private string _text;
+    private bool _isHidden;
+
+    public Word(string text)
+    {
+        _text = text;
+    }
+    public void Hide()
+    {
+
+    }
+    public void Show()
+    {
+
+    }
+    public bool IsHidden()
+    {
+        return [];
+    }
+    public string GetDisplayText()
+    {
+        return [];
+    }
+}
