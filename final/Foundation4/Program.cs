be2f4de107c01544cf889e7d7a406134e7cf8954
kandl25 <war21039@byui.edu>
2024-07-19 10:20:58 -0600

    Exercise

--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -8,7 +8,7 @@ public class Program
         List<Activity> activities = new List<Activity>
         {
             new Running("03 Jul 2024", 30, 3.0),
-            new Cycling("04 Jul 2024", 45, 15.0),
+            new Cycling("04 Jul 2024", 100, 15.0),
             new Swimming("05 Jul 2024", 60, 40)
         };
 
